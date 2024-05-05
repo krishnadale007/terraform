6225f08ee59bcd420163a55d0885784583a345ec
krishnadale007 <krishnadale007@gmail.com>
2024-05-05 15:24:38 +0530

    stop

--- instance.tf
+++ instance.tf
@@ -1,22 +1,22 @@
-terraform {
-    required_providers {
-      aws  = {
-        source = "hashicorp/aws"
-        version = "5.40.0"
-      }
-    }
-}
-provider "aws" {
-    region = "ap-south-1"
-    #shared_config_files = ["/home/ubuntu/.aws/config"]
-    #shared_credentials_files = ["/home/ubuntu/.aws/credentials"] 
-    #profile = "krishna" 
-}
-resource "aws_instance" "ubuntu_server"{
-ami = "ami-0f58b397bc5c1f2e8"
-  instance_type = "t2.micro"
-  key_name = "mumbai"
-  tags = {
-    Name ="ubuntu_srevr"
-  }
-}
+# terraform {
+#     required_providers {
+#       aws  = {
+#         source = "hashicorp/aws"
+#         version = "5.40.0"
+#       }
+#     }
+# }
+# provider "aws" {
+#     region = "ap-south-1"
+#     #shared_config_files = ["/home/ubuntu/.aws/config"]
+#     #shared_credentials_files = ["/home/ubuntu/.aws/credentials"] 
+#     #profile = "krishna" 
+# }
+# resource "aws_instance" "ubuntu_server"{
+# ami = "ami-0f58b397bc5c1f2e8"
+#   instance_type = "t2.micro"
+#   key_name = "mumbai"
+#   tags = {
+#     Name ="ubuntu_srevr"
+#   }
+# }
