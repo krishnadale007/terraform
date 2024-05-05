786de60910fff79500c35f40bd0a09a800fdf519
krishnadale007 <krishnadale007@gmail.com>
2024-05-05 13:46:58 +0530

    ok

--- instance.tf
+++ instance.tf
@@ -8,9 +8,9 @@ terraform {
 }
 provider "aws" {
     region = "ap-south-1"
-    shared_config_files = ["/home/ubuntu/.aws/config"]
-    shared_credentials_files = ["/home/ubuntu/.aws/credentials"] 
-    profile = "krishna" 
+    #shared_config_files = ["/home/ubuntu/.aws/config"]
+    #shared_credentials_files = ["/home/ubuntu/.aws/credentials"] 
+    #profile = "krishna" 
 }
 resource "aws_instance" "ubuntu_server"{
 ami = "ami-0f58b397bc5c1f2e8"
