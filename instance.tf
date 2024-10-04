d657b6e3505b101461b3d02dc86fae05ba34d55b
adaleshri <adaleshree@gmail.com>
2024-10-04 13:05:52 +0530

    all

--- instance.tf
+++ instance.tf
@@ -1,15 +1,15 @@
-provider "aws" {
-    region = "ap-south-1"
+# provider "aws" {
+#     region = "ap-south-1"
   
-}
-resource "aws_instance" "ubuntu_servre" {
-    ami = "ami-078264b8ba71bc45e"
-    key_name = "krish.pem"
-    count = 1
-    instance_type = "t2.micro"
-  # instance of mumbai region
-}
-output "created_server" {
-    value = "yes"
+# }
+# resource "aws_instance" "ubuntu_servre" {
+#     ami = "ami-078264b8ba71bc45e"
+#     key_name = "krish.pem"
+#     count = 1
+#     instance_type = "t2.micro"
+#   # instance of mumbai region
+# }
+# output "created_server" {
+#     value = "yes"
   
-}
+# }
