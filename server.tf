4efa8dd36e2378ffc97988c7b3191e7fdb0c75b8
krishnadale007 <krishnadale007@gmail.com>
2025-04-17 00:23:44 +0530

    gg

--- server.tf
+++ server.tf
@@ -1,52 +1,67 @@
-# provider "aws" {
-#   region = "ap-south-1"  # or your desired region
-#   shared_config_files = [ "/home/ubuntu/.aws/config" ]
-#   shared_credentials_files = [ "/home/ubuntu/.aws/credentials" ]
-#   profile = "krishna"  
-# }
-# # Security Group
-# resource "aws_security_group" "mysecurity_group" {
-#   name        = "automate_sg"
-#   description = "this will add a TF generated security group"
-#   vpc_id      = aws_default_vpc.default.id
-# #keypair create
-# resource aws_key_pair my_key {
-#     key_name = "riddhi_ec2"
-#     public_key =file("riddhi_ec2.pub")
-  
-#   ingress {
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#     description = "SSH"
-#   }
+provider "aws" {
+  region                  = "ap-south-1" # or your desired region
+  shared_config_files     = ["/home/ubuntu/.aws/config"]
+  shared_credentials_files = ["/home/ubuntu/.aws/credentials"]
+  profile                 = "krishna" # profile
+}
 
-#   ingress {
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#     description = "HTTP"
-#   }
+# Key pair creation
+resource "aws_key_pair" "my_key" {
+  key_name   = "riddhi_ec2"
+  public_key = file("riddhi_ec2.pub")
+}
 
-#   egress {
-#     from_port   = 0
-#     to_port     = -1
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#     description = "All access open port"
-#   }
+# VPC & Security Group
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
 
-#   tags = {
-#     name = "automate_sg"
-#   }
-# }
+resource "aws_security_group" "security_group_1" {
+  name        = "automate_sg"
+  description = "this will add a TF generated security group"
+  vpc_id      = aws_default_vpc.default.id # interpolation
 
-# # EC2 Instance - This must be outside the security group block!
-# resource "aws_instance" "my_instance" {
-#   ami             = "ami-0e35ddab05955cf57"
-#   instance_type   = "t2.micro"
-#   key_name        = aws_key_pair.my_key.key_name
-#   security_groups = [aws_security_group.mysecurity_group.name]
-# }
+  # Inbound rules
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "ssh"
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "http"
+  }
+
+  # Outbound rules
+  egress {
+    from_port   = 0
+    to_port     = 0 # Changed to 0 to represent all ports for all protocols
+    protocol    = "-1" # -1 represents all protocols
+    cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
+    description = "all access open port"
+  }
+
+  tags = {
+    Name = "automate_sg"
+  }
+} # Removed the extra closing brace here
+
+# EC2 instance creation
+resource "aws_instance" "my_instance" {
+  ami             = "ami-0e35ddab05955cf57"
+  instance_type   = "t2.micro"
+  key_name        = aws_key_pair.my_key.key_name
+  security_groups = [aws_security_group.security_group_1.name]
+
+  tags = {
+    Name = "riddhi_instance"
+  }
+}
