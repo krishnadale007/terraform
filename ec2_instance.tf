cbea4bc303425f7eaaa2c5e057a1d3e249bf17e4
krishnadale007 <152286255+krishnadale007@users.noreply.github.com>
2025-04-17 00:01:49 +0530

    Update ec2_instance.tf

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -1,54 +1,60 @@
-# #keypair create
-# resource aws_key_pair my_key {
-#     key_name = "riddhi_ec2"
-#     public_key =file("riddhi_ec2.pub")
+provider "aws" {
+  region = "ap-south-1"  # or your desired region
+  shared_config_files = [ "/home/ubuntu/.aws/config" ]
+  shared_credentials_files = [ "/home/ubuntu/.aws/credentials" ]
+  profile = "krishna"  # profile
+}
+ #keypair create
+ resource aws_key_pair my_key {
+     key_name = "riddhi_ec2"
+     public_key =file("riddhi_ec2.pub")
   
-# }
-# #vpc & security group
-# resource "aws_default_vpc" "default" {
-#   tags = {
-#     Name = "Default VPC"
-#   }
-# }
-# resource "aws_security_group" "security_group_1" {
-#     name = "automate_sg"
-#     description = "this will add a TF genrated security group"
-#     vpc_id = aws_default_vpc.default.id #interpolation
+ }
+ #vpc & security group
+ resource "aws_default_vpc" "default" {
+   tags = {
+     Name = "Default VPC"
+   }
+ }
+ resource "aws_security_group" "security_group_1" {
+    name = "automate_sg"
+    description = "this will add a TF genrated security group"
+    vpc_id = aws_default_vpc.default.id #interpolation
     
-#     #inbound rules
-#     ingress{
-#         from_port = 22
-#         to_port = 22
-#         protocol = "tcp"
-#         cidr_blocks = ["0.0.0.0/0"]
-#         description = "ssh"
-#     }
-#     ingress{
-#         from_port = 80              
-#         to_port = 80
-#         protocol = "tcp"
-#         cidr_blocks = ["0.0.0.0/0"]
-#         description = "http"
-#     }
+    #inbound rules
+    ingress{
+         from_port = 22
+          to_port = 22
+        protocol = "tcp"
+       cidr_blocks = ["0.0.0.0/0"]
+         description = "ssh"
+     }
+    ingress{
+         from_port = 80              
+         to_port = 80
+         protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+         description = "http"
+     }
 
 # #outbond rules
-# egress{
-#     from_port = 0
-#     to_port = -1
-#     protocol = "tcp"
-#     description = "all access open port"
-# }
-# tags = {
-#     name ="automate_sg"
-# }
+  egress{
+     from_port = 0
+    to_port = -1
+     protocol = "tcp"
+     description = "all access open port"
+ }
+ tags = {
+     name ="automate_sg"
+ }
 
-# #ecreate ec2 
-# resource "aws_instance" "my_instance" {
-#   ami             = "ami-0e35ddab05955cf57"
-#   instance_type   = "t2.micro"
-#   key_name        = aws_key_pair.my_key.key_name
-#   security_groups = [aws_security_group.security_group_1.name]
-# }
+#ecreate ec2 
+  resource "aws_instance" "my_instance" {
+  ami             = "ami-0e35ddab05955cf57"
+  instance_type   = "t2.micro"
+   key_name        = aws_key_pair.my_key.key_name
+   security_groups = [aws_security_group.security_group_1.name]
+ }
 
 
-# }
+}
